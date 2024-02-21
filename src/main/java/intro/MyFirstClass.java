de2367049ee0db30fc5bcb09475a191a6eb3737a
Ibraim Asanov <iasanov@nemetschek.bg>
2024-02-21 19:46:13 +0200

    Initial commit

---
+++ src/main/java/intro/MyFirstClass.java
@@ -0,0 +1,13 @@
+package intro;
+
+public class MyFirstClass {
+    //static method
+    public static void printHello(){
+        System.out.println("Hello!");
+    }
+
+    //Main method i.e. entry point in any class (can be executed)
+    public static void main(String[] args) {
+        printHello();
+    }
+}
